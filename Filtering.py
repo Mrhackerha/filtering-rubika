360ce1437d6d77a1fa1822262fea3ee9fcd16fcc
Mrhacker <95595807+Mrhackerha@users.noreply.github.com>
2022-12-03 01:57:51 +0330

    Create Filtering.py

---
+++ Filtering.py
@@ -0,0 +1,138 @@
+#mrhacker
+from colorama import Fore
+import pyfiglet
+import os
+import time
+import sys
+import datetime
+
+
+dat = (datetime.datetime.today())
+
+
+banner = (Fore.CYAN+"""mrhacker""")
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.1)
+
+os.system("clear")
+
+  
+banner = (Fore.YELLOW+"""      
+ mrhacker virtual god....
+.                                                      .
+      .n                     .             .                n.
+  . .dP                   dP               9b               9b   .
+ 4  qXb         .        dX                 Xb       .      dXp   t
+dX.  9Xb      .dXb     __                     __    dXb.   dXP   .Xb
+9XXb._     _.dXXXXb dXXXXbo.               dXXXXb dXXXXb._   _.dXXP
+ 9XXXXXXXXXXXXXVXXXXXXXXOo.           .oOXXXXXXXXVXXXXXXXXXXXXXXXP
+   9XXXXXXXXXXXXXXX ~   ~ OOO8b   d8OOO ~   ~ XXXXXXXXXXXXXXXXXP
+     9XXXXXP   9XX      *     98v8P      *     XXP   9XXXXXXXP
+      ~~~       9X.          .db|db.          .XP       ~~~
+                  )b.  .dbo.dP' v  9b.odb.  .dX(
+                ,dXXXXXXXXXXXb     dXXXXXXXXXXXb.
+               dXXXXXXXXXXXP'   .    9XXXXXXXXXXXb
+              dXXXXXXXXXXXXb   d|b   dXXXXXXXXXXXXb
+              9XXb'    XXXXXb.dX|Xb.dXXXXX'    dXXP
+                       9XXXXXX(   )XXXXXXP
+                       XXXX X. v .X XXXX
+                        XP^X' b   d' X^XX
+                        X. 9         P )X
+                         b          '  d'
+                         """)
+for bnr in banner:
+        sys.stdout.write(bnr)
+        sys.stdout.flush()
+        time.sleep(0.004)
+
+
+        
+print("")        
+print("")        
+print("")        
+print("")        
+        
+print(Fore.GREEN,dat)
+
+                
+print(Fore.BLUE+pyfiglet.figlet_format('MRHACKER))
+print("                    Mrhacker")
+
+print("")
+print("")
+print("")
+print("")
+time.sleep(1.7)
+print (Fore.RED+"MRHACKER")
+time.sleep(0.5)
+print ("CHANNEL RUBIKA: @HAZRAT_RUBIKA")
+time.sleep(0.4)
+print ("09376785910")
+time.sleep(0.3)
+print("")
+print("")
+
+
+up = input(Fore.WHITE+""" 
+[dxprit (1) ]
+[bug-Algorithm (2) ]
+[antifilter web porn  (3) ]         
+Which do you want>>>""")
+
+
+print(Fore.CYAN+"\rscanning..",end="",flush=False)
+time.sleep(1)
+print("\rscanning....",end="",flush=False)
+time.sleep(1)
+print("\rscanning.....",end="",flush=False)
+time.sleep(1)
+print("\rscanning......",end="",flush=False)
+time.sleep(1)
+print("\rscanning.......",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning.........",end="",flush=False)
+time.sleep(0.5)
+print("\rscanning..........",end="",flush=False)
+time.sleep(1)
+
+
+print("")
+print("")
+print("")
+
+
+if up == "1":
+         print ("""  \033[41m(http://dxprit-filtering-rubika-174.gigfa.com/dxprit.html)\33[1;0m  """)
+   
+         
+                     
+if up == "2":
+         print ("""  \033[41m('(/8.8.8.4/f/h.u//6.1.5.9.2/y//d/f/6.5.1.9.3.4.8.9.7.3.4.6.6.4.1.4.3.6.9.1.8.5.9.6.7.1.1.1.3.7.6/)')\33[1;0m   """)
+         
+ 
+                            
+if up == "3":
+        print("""" \033[41(https://xa.fltvuq.cn)\33[1;0m  """)   
+        
+time.sleep(1)
+time.sleep(0.3)
+print (" Cod Ro 10 bar Bezanin Lotfa Va Dorost Cod Ro Bezanin Ta Fake Nasheh ")
+print(" ")
+print (" ")
+time.sleep(0.6)
+time.sleep(1.7)
+print ("                    Godrat Majazi Mnm")
+time.sleep(1.0)
+print ("")
+time.sleep(0.5)
+print ("   Payambar Salatin")
+time.sleep(0.1)
+print ("   ")
+time.sleep(0.2)
+print (""" mrhacker 
+""")y
+""")
